c4adccff0eae6f0bcdefb09a105b4df99cbbe728
Gianni Steyn <giannisteyn0604@gmail.com>
2026-06-10 11:33:28 +0200

    Commit and Push

--- app/src/main/java/com/example/campsitecommander/MainActivity.kt
+++ app/src/main/java/com/example/campsitecommander/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.campsitecommander
 
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.text.Layout
+import android.widget.ArrayAdapter
+import android.widget.Button
 import android.widget.EditText
 import android.widget.LinearLayout
 import android.widget.ScrollView
@@ -21,31 +24,102 @@ import com.example.campsitecommander.ui.theme.CampsiteCommanderTheme
 import org.w3c.dom.Comment
 
 class MainActivity : ComponentActivity() {
-   private lateinit var splashLayout: LinearLayout
+    private lateinit var splashLayout: LinearLayout
 
-   private lateinit var mainLayout: ScrollView
+    private lateinit var mainLayout: ScrollView
 
-   private lateinit var detailsLayout: ScrollView
+    private lateinit var detailsLayout: ScrollView
 
-   private lateinit var txtTotalItemPacked: TextView
+    private lateinit var txtTotalItemPacked: TextView
 
-   private lateinit var txtDetails: TextView
+    private lateinit var txtDetails: TextView
 
-   private lateinit var editCategory: EditText
+    private lateinit var editCategory: EditText
 
-   private lateinit var editQuantity: EditText
+    private lateinit var editQuantity: EditText
 
-   private lateinit var editComments: EditText
+    private lateinit var editComments: EditText
 
-   private lateinit var spinnerItems: Spinner
+    private lateinit var spinnerItems: Spinner
 
-   private val items = arrayOf(
-       "Tent", "Marshmallows", "Flashlight"
-   )
+    private val items = arrayOf(
+        "Tent", "Marshmallows", "Flashlight"
+    )
 
-   private val category = IntArray(3 )
+    private val category = IntArray(3)
 
-   private val quantity = IntArray(3 )
+    private val quantity = IntArray(3)
+
+    private val comments = Array(3) { "" }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        splashLayout = findViewById(R.id.splashLayout)
+        mainLayout = findViewById(R.id.mainLayout)
+        detailsLayout = findViewById(R.id.detailsLayout)
+        txtTotalItemPacked = findViewById(R.id.txtTotalItems)
+        txtDetails = findViewById(R.id.txtDetails)
+        editCategory = findViewById(R.id.editCategory)
+        editQuantity = findViewById(R.id.editQuantity)
+        editComments = findViewById(R.id.editComments)
+        spinnerItems = findViewById(R.id.spinnerItems)
+        val adapter = ArrayAdapter(
+            this,
+            android.R.layout.simple_spinner_dropdown_item,
+            items
+        )
+        spinnerItems.adapter = adapter
+
+        // Buttons
+        val btnStart = findViewById<Button>(R.id.btnStart)
+        val btnSave = findViewById<Button>(R.id.btnSave)
+        val btnTotal = findViewById<Button>(R.id.btnTotal)
+        val btnDetails = findViewById<Button>(R.id.btnDetails)
+        val btnClear = findViewById<Button>(R.id.btnClear)
+        val btnBack = findViewById<Button>(R.id.btnBack)
+        val btnExit = findViewById<Button>(R.id.btnExit)
+
+        // Splash Screen Navigation
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
